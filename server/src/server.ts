fed5e5c4a0441506656bd3f62bece5e0c2e0ba59
Dan Fincher <danbfjr@gmail.com>
2025-11-10 20:49:15 -0500

    Add QOL updates. Add morgan for network traffic logging. Print network IP

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,22 +1,29 @@
 import express from "express";
 import cors from "cors";
 import fetch from "node-fetch";
+import dotenv from 'dotenv';
+import morgan from "morgan";
+import https from "https";
+import fs from "fs";
+import { getLocalIpAddress } from "./utilities/getLocalIpAddress";
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
 
-// CORS configuration
-const corsOptions = {
-  origin: ["http://localhost:5173", "https://localhost:5173"],
-  methods: ["GET", "POST", "OPTIONS"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-  credentials: true,
-  optionsSuccessStatus: 204
-};
-
 // Middleware
-app.use(cors(corsOptions));
+// Temporary CORS configuration for debugging
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
+
+app.use(morgan("tiny"));
 app.use(express.json());
+
 
 // Routes
 app.get("/", (req, res) => {
@@ -41,7 +48,18 @@ app.get("/api/movie_info", async (req, res) => {
   }
 });
 
-// Start the server
-app.listen(PORT, () => {
-  console.log(`Server is running`);
+const options = {
+  key: fs.readFileSync("./server.key"),
+  cert: fs.readFileSync("./server.cert"),
+};
+
+const localIp = getLocalIpAddress();
+https.createServer(options, app).listen(PORT, () => {
+  console.log(`Server is running on https://localhost:${PORT}`);
+  if (localIp) {
+    console.log(`Server is also accessible on https://${localIp}:${PORT}`);
+  }
 });
+
+// For self signed cert generation:
+// openssl req -nodes -new -x509 -keyout server.key -out server.cert -days 365
